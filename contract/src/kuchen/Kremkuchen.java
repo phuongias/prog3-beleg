9d1a9bcbb09cd43b704f32c1cda187ee65405db8
phuongias <phng2912@gmail.com>
2024-04-04 23:55:05 +0200

    DL Impl Unit Tests

--- contract/src/kuchen/Kremkuchen.java
+++
@@ -1,7 +0,0 @@
-package kuchen;
-
-import verwaltung.Verkaufsobjekt;
-
-public interface Kremkuchen extends Kuchen, Verkaufsobjekt {
-    String getKremsorte();
-}
--- domainLogic/src/observerPattern/Observable.java
+++
@@ -1,9 +0,0 @@
-package observerPattern;
-
-public interface Observable {
-    boolean register(Observer observer);
-    boolean unregister(Observer observer);
-    void notifyObserver();
-
-}
-
